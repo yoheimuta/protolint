c738f6dbb2cc9e540f922f98762b7fe5533dfabe
yoheimuta <yoheimuta@gmail.com>
2018-12-22 11:05:55 +0900

    tests: Fixed GO111MODULE

--- .circleci/install_dep.sh
+++ .circleci/install_dep.sh
@@ -2,11 +2,11 @@
 
 set -euxo pipefail
 
-GO111MODULE=on go get -u golang.org/x/tools/cmd/goimports
+go get -u golang.org/x/tools/cmd/goimports
 go get -u golang.org/x/lint/golint
 go get -u github.com/kisielk/errcheck
-go get -u github.com/haya14busa/gosum/cmd/gosumcheck
-go get -u github.com/gordonklaus/ineffassign
-go get -u github.com/opennota/check/cmd/varcheck
-go get -u github.com/opennota/check/cmd/aligncheck
-go get -u github.com/mdempsky/unconvert
+GO111MODULE=off go get -u github.com/haya14busa/gosum/cmd/gosumcheck
+GO111MODULE=off go get -u github.com/gordonklaus/ineffassign
+GO111MODULE=off go get -u github.com/opennota/check/cmd/varcheck
+GO111MODULE=off go get -u github.com/opennota/check/cmd/aligncheck
+GO111MODULE=off go get -u github.com/mdempsky/unconvert
