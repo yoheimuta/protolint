648393c616a9fbbee183024c8f98e6192be23eac
yoheimuta <yoheimuta@gmail.com>
2019-01-09 18:39:26 +0900

    tests: Added pretty print function

---
+++ internal/util_test/pretty.go
@@ -0,0 +1,15 @@
+package util_test
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// PrettyFormat formats any objects for test debug use..
+func PrettyFormat(v interface{}) string {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		return fmt.Sprintf("orig=%v, err=%v", v, err)
+	}
+	return string(b)
+}
