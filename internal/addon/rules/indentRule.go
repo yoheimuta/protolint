3aac051b627b0486ff5eae626b47d24d486a2f30
yoheimuta <yoheimuta@gmail.com>
2019-08-21 13:54:54 +0900

    fix: INDENT rule handle 'extend' properly

--- internal/addon/rules/indentRule.go
+++ internal/addon/rules/indentRule.go
@@ -131,6 +131,22 @@ func (v indentVisitor) VisitEnumField(f *parser.EnumField) (next bool) {
 	return false
 }
 
+func (v indentVisitor) VisitExtend(e *parser.Extend) (next bool) {
+	v.validateIndent(e.Meta.Pos)
+	if e.Meta.Pos.Line < e.Meta.LastPos.Line {
+		v.validateIndent(e.Meta.LastPos)
+	}
+	for _, comment := range e.Comments {
+		v.validateIndent(comment.Meta.Pos)
+	}
+
+	defer v.nest()()
+	for _, body := range e.ExtendBody {
+		body.Accept(v)
+	}
+	return false
+}
+
 func (v indentVisitor) VisitField(f *parser.Field) (next bool) {
 	v.validateIndent(f.Meta.Pos)
 	for _, comment := range f.Comments {
