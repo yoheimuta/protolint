64d399007e2ae2de0cc525b8af545d928ef1de97
yoheimuta <yoheimuta@gmail.com>
2021-01-17 16:48:14 +0900

    chore: Add a note about sh dependency

--- _example/plugin/main.go
+++ _example/plugin/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/yoheimuta/protolint/_example/plugin/customrules"
 	"github.com/yoheimuta/protolint/internal/addon/rules"
 	"github.com/yoheimuta/protolint/linter/rule"
 	"github.com/yoheimuta/protolint/plugin"
 )
 
+var (
+	goStyle = flag.Bool("go_style", true, "the comments should follow a golang style")
+)
+
 func main() {
+	flag.Parse()
+
 	plugin.RegisterCustomRules(
 		// The purpose of this line just illustrates that you can implement the same as internal linter rules.
-		rules.NewEnumsHaveCommentRule(true),
+		rules.NewEnumsHaveCommentRule(*goStyle),
 
 		// A common custom rule example. It's simple.
 		customrules.NewEnumNamesLowerSnakeCaseRule(),
