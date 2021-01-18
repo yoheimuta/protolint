c96570705f42aab4fa85a99e68d8180451a8bc50
yoheimuta <yoheimuta@gmail.com>
2021-01-18 10:46:21 +0900

    feat: Ignore a windows path by a config of unix path

--- internal/stringsutil/slice.go
+++ internal/stringsutil/slice.go
@@ -1,5 +1,7 @@
 package stringsutil
 
+import "github.com/yoheimuta/protolint/internal/filepathutil"
+
 // ContainsStringInSlice searches the haystack for the needle.
 func ContainsStringInSlice(
 	needle string,
@@ -7,6 +9,19 @@ func ContainsStringInSlice(
 ) bool {
 	for _, h := range haystack {
 		if h == needle {
+			return true
+		}
+	}
+	return false
+}
+
+// ContainsCrossPlatformPathInSlice searches the unix path haystack for the cross-platform needle.
+func ContainsCrossPlatformPathInSlice(
+	needle string,
+	unixPathHaystack []string,
+) bool {
+	for _, h := range unixPathHaystack {
+		if filepathutil.IsSameUnixPath(h, needle) {
 			return true
 		}
 	}
